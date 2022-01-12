618754da936e61f5fca4cc05a191eb871d662e63
Ranisans <ranisans@yahoo.com>
2022-01-12 16:10:56 +0300

    feat: add ErrorPage and Spinner

---
+++ src/components/ErrorPage/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './index.css';
+
+export default function ErrorPage() {
+  return (
+    <div className="error-page">
+      Sorry, something went wrong!
+      <br />
+      Please try again.
+    </div>
+  );
+}
